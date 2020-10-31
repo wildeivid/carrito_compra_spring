af8d2c633e84c349fff26125552c57fa8a332965
Wilson Cordero <captmus.wildeivid@gmail.com>
2020-10-31 00:57:06 -0500

    se crean Entity requeridos

---
+++ carrito-compra-spring/src/main/java/com/carritocompra/app/models/entity/Carrito.java
@@ -0,0 +1,72 @@
+package com.carritocompra.app.models.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "carts")
+public class Carrito implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	private String status;
+	
+	@ManyToOne
+	private Usuario usuario;
+	
+	@OneToMany(targetEntity = ProductoCarrito.class)
+	private List<ProductoCarrito> listaProductoCarrito;
+	
+	
+	
+
+	
+	
+	
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+
+}
