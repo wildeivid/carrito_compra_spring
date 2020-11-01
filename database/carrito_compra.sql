e19a90cac062bf9af81f6f462959f08f66ad4f42
Wilson Cordero <captmus.wildeivid@gmail.com>
2020-11-01 01:19:37 -0500

    se culmina 2da parte del proyecto al agregar la administracion de usuarios

--- database/carrito_compra.sql
+++ database/carrito_compra.sql
@@ -1,33 +1,14 @@
--- phpMyAdmin SQL Dump
--- version 5.0.3
--- https://www.phpmyadmin.net/
---
--- Servidor: localhost
--- Tiempo de generación: 31-10-2020 a las 08:29:17
--- Versión del servidor: 10.4.14-MariaDB
--- Versión de PHP: 7.2.34
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Base de datos: `carrito_compra`
---
 CREATE DATABASE IF NOT EXISTS `carrito_compra` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `carrito_compra`;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `carts`
---
 
 CREATE TABLE IF NOT EXISTS `carts` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -37,24 +18,12 @@ CREATE TABLE IF NOT EXISTS `carts` (
   KEY `FKhjykljcwleunj1rmbji204j34` (`usuario_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `carts_lista_producto_carrito`
---
-
 CREATE TABLE IF NOT EXISTS `carts_lista_producto_carrito` (
   `carrito_id` bigint(20) NOT NULL,
   `lista_producto_carrito_id` bigint(20) NOT NULL,
   UNIQUE KEY `UK_77ibp89hmeb9bt20lj8rgysqr` (`lista_producto_carrito_id`),
   KEY `FK1sq18gwxbq36q9j0usjbum5xi` (`carrito_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `products`
---
 
 CREATE TABLE IF NOT EXISTS `products` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -63,10 +32,6 @@ CREATE TABLE IF NOT EXISTS `products` (
   `sku` decimal(19,2) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `products`
---
 
 INSERT INTO `products` (`id`, `descripcion`, `nombre`, `sku`) VALUES(1, 'Televisor', 'TV Samsung 24\"', '700999.99');
 INSERT INTO `products` (`id`, `descripcion`, `nombre`, `sku`) VALUES(2, 'Bicicleta eléctrica de alta revolución', 'BIKE ELECTRIC PLUS', '1999999.00');
@@ -79,12 +44,6 @@ INSERT INTO `products` (`id`, `descripcion`, `nombre`, `sku`) VALUES(8, 'Escrito
 INSERT INTO `products` (`id`, `descripcion`, `nombre`, `sku`) VALUES(9, 'Impresora HP Tinta Contínua', 'Impresora HP Tinta Contínua', '455000.00');
 INSERT INTO `products` (`id`, `descripcion`, `nombre`, `sku`) VALUES(10, 'Impresora Samsung Láser', 'Impresora Samsung Láser', '799999.00');
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `product_cars`
---
-
 CREATE TABLE IF NOT EXISTS `product_cars` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `quantity` int(11) NOT NULL,
@@ -94,12 +53,6 @@ CREATE TABLE IF NOT EXISTS `product_cars` (
   KEY `FKgp88jltqi5upkgcwqw2d3b0ms` (`carrito_id`),
   KEY `FKnwpl0aaghs2xanht254la1wd4` (`producto_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuario`
---
 
 CREATE TABLE IF NOT EXISTS `usuario` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -112,18 +65,8 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   UNIQUE KEY `UK_pimirdqbf828ejsgnxmc3kfix` (`user_name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
---
--- Volcado de datos para la tabla `usuario`
---
-
 INSERT INTO `usuario` (`id`, `apellido`, `email`, `fecha_nacimiento`, `nombre`, `user_name`) VALUES(1, 'CORDERO', 'wildeivid7@gmail.com', '1990-07-02', 'WILSON', 'wildeivid');
 INSERT INTO `usuario` (`id`, `apellido`, `email`, `fecha_nacimiento`, `nombre`, `user_name`) VALUES(2, 'CHACÓN', 'danielavirginia7@gmail.com', '1993-06-29', 'DANIELA', 'UDANI');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuario_lista_carritos`
---
 
 CREATE TABLE IF NOT EXISTS `usuario_lista_carritos` (
   `usuario_id` bigint(20) NOT NULL,
@@ -132,33 +75,18 @@ CREATE TABLE IF NOT EXISTS `usuario_lista_carritos` (
   KEY `FKon0tls0nxukk807l9t0ltk5xy` (`usuario_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Restricciones para tablas volcadas
---
 
---
--- Filtros para la tabla `carts`
---
 ALTER TABLE `carts`
   ADD CONSTRAINT `FKhjykljcwleunj1rmbji204j34` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`);
 
---
--- Filtros para la tabla `carts_lista_producto_carrito`
---
 ALTER TABLE `carts_lista_producto_carrito`
   ADD CONSTRAINT `FK1sq18gwxbq36q9j0usjbum5xi` FOREIGN KEY (`carrito_id`) REFERENCES `carts` (`id`),
   ADD CONSTRAINT `FKr9q5v9sqk1c20ukhu7x721776` FOREIGN KEY (`lista_producto_carrito_id`) REFERENCES `product_cars` (`id`);
 
---
--- Filtros para la tabla `product_cars`
---
 ALTER TABLE `product_cars`
   ADD CONSTRAINT `FKgp88jltqi5upkgcwqw2d3b0ms` FOREIGN KEY (`carrito_id`) REFERENCES `carts` (`id`),
   ADD CONSTRAINT `FKnwpl0aaghs2xanht254la1wd4` FOREIGN KEY (`producto_id`) REFERENCES `products` (`id`);
 
---
--- Filtros para la tabla `usuario_lista_carritos`
---
 ALTER TABLE `usuario_lista_carritos`
   ADD CONSTRAINT `FKnnptxfs2x8535esdlq1kwr43w` FOREIGN KEY (`lista_carritos_id`) REFERENCES `carts` (`id`),
   ADD CONSTRAINT `FKon0tls0nxukk807l9t0ltk5xy` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`);
